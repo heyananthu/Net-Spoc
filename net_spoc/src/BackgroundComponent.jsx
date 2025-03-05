23ae27008b3edab8ba7c5c4850fb4470295a177f
ananthu <heyananthulal@gmail.com>
2025-03-05 16:37:06 +0530

    second commit

--- net_spoc/src/BackgroundComponent.jsx
+++ net_spoc/src/BackgroundComponent.jsx
@@ -5,20 +5,20 @@ import india from "./assets/india.png";
 
 function BackgroundComponent() {
     return (
-        <div className="absolute inset-0 w-full h-fit bg-black overflow-hidden pointer-events-none -z-10">
-            <div className="relative  flex justify-between">
+        <div className="absolute inset-0 w-full h-screen bg-black overflow-hidden pointer-events-none -z-10">
+            <div className="relative flex justify-between h-full">
                 {/* Left Image */}
                 <img
                     src={leftimage}
-                    className="w-[35%] max-w-[43rem] h-screen md:h-screen object-cover"
+                    className="w-[35%] max-w-[43rem] h-full object-cover"
                     alt="Left Background"
                 />
 
                 {/* Center Image (India) */}
-                <div className="flex justify-center items-end h-[30rem] pb-12">
+                <div className="absolute inset-0 flex justify-center items-end">
                     <img
                         src={india}
-                        className="w-[18rem] md:w-[23rem] absolute md:top-[18rem] top-[22rem] sm:left-[30%] left-[7rem] md:left-[38%] z-10"
+                        className="w-[12rem] md:w-[15.4rem] max-h-full"
                         alt="India"
                     />
                 </div>
@@ -26,7 +26,7 @@ function BackgroundComponent() {
                 {/* Right Image */}
                 <img
                     src={rightimage}
-                    className="md:block w-[35%] max-w-[43rem] h-screen md:h-screen object-cover"
+                    className="w-[35%] max-w-[43rem] h-full object-cover"
                     alt="Right Background"
                 />
             </div>
