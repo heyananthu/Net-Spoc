bffc343763fcced8cb40f8f21dde78909190c183
ananthu <heyananthulal@gmail.com>
2025-03-12 13:43:29 +0530

    made some  correction

--- net_spoc/src/BackgroundComponent.jsx
+++ net_spoc/src/BackgroundComponent.jsx
@@ -31,6 +31,42 @@ function BackgroundComponent() {
                 />
             </div>
         </div>
+
+
+
+
+
+        // <div className="bg-black absolute inset-0 w-full h-full overflow-hidden -z-10">
+        //     <div className="relative flex justify-between h-full">
+        //         {/* Left Image */}
+        //         <div className="hidden md:block ">
+        //             <img src={leftimage} className="w-full h-screen object-cover" alt="Left" />
+        //         </div>
+                
+        //         {/* Center Content */}
+        //         <div className='flex flex-col justify-center items-center text-white mt-24 space-y-6 w-full text-center p-6'>
+        //             <h1 className="font-audiowide font-normal text-3xl md:text-5xl">
+        //                 Elevating Connectivity,<br className="hidden md:block" /> Redefining Excellence.
+        //             </h1>
+        //             <p className='text-sm md:text-base font-extralight font-poppins text-gray-100'>
+        //                 Experience the NetSpoc difference—seamless connectivity with reliability <br className="hidden md:block" />
+        //                 and transparency. Stay confidently connected.
+        //             </p>
+        //             {/* India Map Image Below Paragraph */}
+        //             <div className="flex justify-center items-center w-full mt-6">
+        //                 <img src={india} className="w-[70%] sm:w-[50%] md:w-[50%] lg:w-[40%]" alt="India" />
+        //             </div>
+        //         </div>
+                
+        //         {/* Right Image */}
+        //         <div className="hidden md:block">
+        //             <img src={rightimage} className="w-full h-screen object-cover" alt="Right" />
+        //         </div>
+        //     </div>
+        // </div>
+
+
+
     );
 }
 
--- net_spoc/src/Components/LangingPage/HeroSection.jsx
+++ net_spoc/src/Components/LangingPage/HeroSection.jsx
@@ -6,13 +6,13 @@ import MapofIndia from '../../assets/Map of India.png'
 function HeroSection() {
     return (
         <div className='w-full h-full text-white space-y-6 '>
-            <div className='text-center space-y-4 mt-12 p-6'>
-                <h1 class="font-audiowide font-normal text-3xl md:text-6xl text-center">Elevating Connectivity,<br /> Redefining Excellence.</h1>
-                <p className='text-[13px] font-poppins text-gray-100'>Experience the NetSpoc difference—seamless connectivity with reliability <br />and transparency. Stay confidently connected.</p>
+            <div className='text-center space-y-4 mt-12 md:mt-5 p-6'>
+                <h1 class="font-audiowide font-normal text-2xl md:text-6xl text-center">Elevating Connectivity,<br /> Redefining Excellence.</h1>
+                <p className='md:text-[13px] text-[10px] font-poppins font-light text-gray-100'>Experience the NetSpoc difference—seamless connectivity with reliability <br />and transparency. Stay confidently connected.</p>
             </div>
 
             <div className='flex justify-center'>
-                <img src={india} className='md:w-[25rem] md:h-[29rem] w-[21rem] h-[25rem] translate-x-6 md:translate-0 md:-translate-y-5 ' />
+                <img src={india} className='md:w-[24rem] md:h-[28rem] w-[21rem] h-[25rem] translate-x-6 md:translate-0 md:-translate-y-12 ' />
             </div>
             {/* <div>
                 <img src={leftvector1} className='absolute top-[23rem]  w-[41rem]' />
