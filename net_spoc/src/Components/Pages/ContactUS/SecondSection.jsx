97243e8ef7231011056a21e88f55e1e6c7532c18
ananthu <heyananthulal@gmail.com>
2025-03-25 10:24:40 +0530

    Contact number edited

--- net_spoc/src/Components/Pages/ContactUS/SecondSection.jsx
+++ net_spoc/src/Components/Pages/ContactUS/SecondSection.jsx
@@ -17,7 +17,7 @@ function SecondSection() {
                         </div>
                         <div>
                             <p>WhatsApp Number</p>
-                            <p className='font-bold text-lg'>+91 984 540-5405</p>
+                            <p className='font-bold text-lg'>+91 968 669-7700</p>
                         </div>
                     </div>
                 </div>
